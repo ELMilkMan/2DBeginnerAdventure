669a5bcfbdf13652a44a7594e707cd5e9f7c2382
ELMilkMan <730254@student.dusd.net>
2024-11-07 11:50:14 -0800

    making spikes damggeable
    time stamp 7th video 10:15

--- 2DBeginnerAdventureP4Ruvalcaba/Assets/scrpits/RubyController.cs
+++ 2DBeginnerAdventureP4Ruvalcaba/Assets/scrpits/RubyController.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 5.0f;
+
+    public int maxHealth = 5;
+    public float timeInvincible = 2;
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
+    bool isInvincible;
+    float invincibleTimer;
+    
+
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
     //Start is called before the first frame update
     void Start()
     {
-        rigidbody2d = GetComponent<Rigidbody2D>();  
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        currentHealth = 1;
     }
 
     // Update is called once per frame
@@ -19,13 +33,23 @@ public class RubyController : MonoBehaviour
              horizontal = Input.GetAxis("Horizontal");
              vertical = Input.GetAxis("Vertical");
         
+
+            if(isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+        }
             }
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 5.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 5.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+    }
+   public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
