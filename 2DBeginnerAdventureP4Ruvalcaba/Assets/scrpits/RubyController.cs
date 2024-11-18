4319b7f6a5161be5ddf4fd30495fb64e20eb07df
ELMilkMan <730254@student.dusd.net>
2024-11-18 12:00:07 -0800

    I fished the code for my thingy
    on 9th video

--- 2DBeginnerAdventureP4Ruvalcaba/Assets/scrpits/RubyController.cs
+++ 2DBeginnerAdventureP4Ruvalcaba/Assets/scrpits/RubyController.cs
@@ -7,7 +7,7 @@ public class RubyController : MonoBehaviour
     public float speed = 5.0f;
 
     public int maxHealth = 5;
-    public float timeInvincible = 2;
+    public float timeInvincible = 2.0f;
     public int health { get { return currentHealth; } }
     int currentHealth;
 
@@ -30,13 +30,23 @@ public class RubyController : MonoBehaviour
         currentHealth = maxHealth;
         animator = GetComponent<Animator>();
     }
-
+    
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately (move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
 
         if (isInvincible)
         {
@@ -60,6 +70,8 @@ public class RubyController : MonoBehaviour
     {
         if (amount < 0)
         {
+            animator.SetTrigger("Hit");
+
             if (isInvincible)
             {
                 return;
